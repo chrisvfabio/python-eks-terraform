b180e58364f68091082fd3d7d44f61f8d4baf6d3
Chris Fabio <chrisfabio@insigtech.com.au>
2023-01-15 21:15:05 +0800

    change region, set initial nodes to 1

--- infra/k8s-helloworld/main.tf
+++ infra/k8s-helloworld/main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-southeast-2"
+  region = "us-east-1"
 }
 
 module "public_eks_cluster" {
@@ -11,8 +11,8 @@ module "public_eks_cluster" {
   source         = "../modules/simple-eks"
   vpc_name       = "upwork-32487863"
   cluster_name   = "k8s-helloworld"
-  desired_size   = 2
-  max_size       = 2
+  desired_size   = 1
   min_size       = 1
+  max_size       = 2
   instance_types = ["t2.small"]
 }
